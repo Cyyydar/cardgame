d8b5167ce5950755e43c70fc15ec16fb74f0cd8e
Cyyydar <mr.nepaharev@mail.ru>
2022-08-20 00:09:18 +0300

    another commit 1

--- card_test.go
+++ card_test.go
@@ -10,6 +10,12 @@ type testDataRemove struct {
 	remove int
 }
 
+type testDataFill struct {
+	data  []Card
+	want  []Card
+	trump Card
+}
+
 func testEq(a, b []Card) bool {
 	if len(a) != len(b) {
 		return false
@@ -22,11 +28,21 @@ func testEq(a, b []Card) bool {
 	return true
 }
 
+func Find(a []Card, x Card) int {
+	for i, n := range a {
+		if x == n {
+			return i
+		}
+	}
+	return len(a)
+}
+
 func TestDeckGenerator(t *testing.T) {
-	deck := make([]Card, 52)
-	DeckGenerator(deck)
+	Deck = make([]Card, 52)
+	DeckGenerator()
 	trueDeck := []Card{{Suit: "heart", Value: 2}, {Suit: "heart", Value: 3}, {Suit: "heart", Value: 4}, {Suit: "heart", Value: 5}, {Suit: "heart", Value: 6}, {Suit: "heart", Value: 7}, {Suit: "heart", Value: 8}, {Suit: "heart", Value: 9}, {Suit: "heart", Value: 10}, {Suit: "heart", Value: 11}, {Suit: "heart", Value: 12}, {Suit: "heart", Value: 13}, {Suit: "heart", Value: 14}, {Suit: "diamond", Value: 2}, {Suit: "diamond", Value: 3}, {Suit: "diamond", Value: 4}, {Suit: "diamond", Value: 5}, {Suit: "diamond", Value: 6}, {Suit: "diamond", Value: 7}, {Suit: "diamond", Value: 8}, {Suit: "diamond", Value: 9}, {Suit: "diamond", Value: 10}, {Suit: "diamond", Value: 11}, {Suit: "diamond", Value: 12}, {Suit: "diamond", Value: 13}, {Suit: "diamond", Value: 14}, {Suit: "club", Value: 2}, {Suit: "club", Value: 3}, {Suit: "club", Value: 4}, {Suit: "club", Value: 5}, {Suit: "club", Value: 6}, {Suit: "club", Value: 7}, {Suit: "club", Value: 8}, {Suit: "club", Value: 9}, {Suit: "club", Value: 10}, {Suit: "club", Value: 11}, {Suit: "club", Value: 12}, {Suit: "club", Value: 13}, {Suit: "club", Value: 14}, {Suit: "spade", Value: 2}, {Suit: "spade", Value: 3}, {Suit: "spade", Value: 4}, {Suit: "spade", Value: 5}, {Suit: "spade", Value: 6}, {Suit: "spade", Value: 7}, {Suit: "spade", Value: 8}, {Suit: "spade", Value: 9}, {Suit: "spade", Value: 10}, {Suit: "spade", Value: 11}, {Suit: "spade", Value: 12}, {Suit: "spade", Value: 13}, {Suit: "spade", Value: 14}}
-	if testEq(deck, trueDeck) {
+	Remove(trueDeck, Find(trueDeck, trumpcard))
+	if testEq(Deck, trueDeck) {
 		return
 	}
 	t.Error("Generation failed")
@@ -56,11 +72,28 @@ func TestRemoveDeck(t *testing.T) {
 }
 
 func TestFill(t *testing.T) {
-	Deck = []Card{{Suit: "heart", Value: 2}, {Suit: "heart", Value: 3}, {Suit: "heart", Value: 4}, {Suit: "heart", Value: 5}, {Suit: "heart", Value: 6}, {Suit: "heart", Value: 7}}
+	//Deck = []Card{{Suit: "heart", Value: 2}, {Suit: "heart", Value: 3}, {Suit: "heart", Value: 4}, {Suit: "heart", Value: 5}, {Suit: "heart", Value: 6}, {Suit: "heart", Value: 7}}
 	//trueHand = []Card{{Suit: "heart", Value: 2}, {Suit: "heart", Value: 3}, {Suit: "heart", Value: 4}, {Suit: "heart", Value: 5}, {Suit: "heart", Value: 6}, {Suit: "heart", Value: 7}}
-	hand := Hand{}
-	hand.Fill()
-	if len(Deck) != 0 || len(hand.Hand) != 6 {
-		t.Error("Deck: ", Deck, "hand: ", hand)
+	// hand := Hand{}
+	// hand.Fill()
+	// if len(Deck) != 0 || len(hand.Hand) != 6 {
+	// 	t.Error("Deck: ", Deck, "hand: ", hand)
+	// }
+	tests := []testDataFill{
+		{[]Card{{Suit: "heart", Value: 2}, {Suit: "heart", Value: 3}, {Suit: "heart", Value: 4}, {Suit: "heart", Value: 5}, {Suit: "heart", Value: 6}, {Suit: "heart", Value: 7}},
+			[]Card{{Suit: "heart", Value: 2}, {Suit: "heart", Value: 3}, {Suit: "heart", Value: 4}, {Suit: "heart", Value: 5}, {Suit: "heart", Value: 6}, {Suit: "heart", Value: 7}},
+			Card{Suit: "heart", Value: 14}},
+		{[]Card{},
+			[]Card{{Suit: "heart", Value: 14}},
+			Card{Suit: "heart", Value: 14}},
+	}
+	for _, test := range tests {
+		Deck = test.data
+		trumpcard = test.trump
+		hand := Hand{}
+		hand.Fill()
+		if !testEq(hand.Hand, test.want) {
+			t.Errorf("TestRemoveDeck() =\n \"%v\" \n, want \n \"%v\" ", hand.Hand, test.want)
+		}
 	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,159 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"math/rand"
+	"net/http"
 	"strconv"
-
-	"github.com/Cyyydar/cards"
+	"time"
 )
+
+// --------------------------------
+var Deck []Card
+var attackTable []Card
+var defenseTable []Card
+var trumpcard Card
+
+func RandomNumber(c int) int {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+	number := rand.Intn(c)
+	return number
+}
+
+func Remove(c []Card, i int) []Card {
+	if i > len(c) {
+		log.Fatal("Мать жива? Ты нафига удаляешь не ту карту")
+	}
+	c = append(c[:i], c[i+1:]...)
+	return c
+}
+
+func DeckGenerator() {
+	suits := []string{"heart", "diamond", "club", "spade"}
+	number := 0
+	for _, suit := range suits {
+		for index, value := number, 2; index < number+13; index++ {
+			Deck[index] = Card{suit, value}
+			value++
+		}
+		number += 13
+	}
+	r := RandomNumber(len(Deck))
+	trumpcard = Deck[r]
+	Remove(Deck, r)
+}
+
+type Card struct {
+	Suit  string
+	Value int
+}
+
+type Hand struct {
+	Hand []Card
+}
+
+// заполняет руку 6 картами
+func (h *Hand) Fill() {
+	if (6-len(h.Hand) < 0 || len(Deck) == 0) && trumpcard != (Card{"", 0}) {
+		h.Hand = append(h.Hand, trumpcard)
+		trumpcard = Card{"", 0}
+		return
+	} else if trumpcard == (Card{"", 0}) {
+		return
+	}
+	for i := len(h.Hand); i < 6; i++ {
+		r := RandomNumber(len(Deck))
+		h.Hand = append(h.Hand, Deck[r])
+		Deck = Remove(Deck, r)
+	}
+}
+
+// Возвращаем мапу номеров карт настоле для подкидывания
+func Table(m map[int]int) {
+	for _, card := range attackTable {
+		m[card.Value]++
+	}
+	for _, card := range defenseTable {
+		m[card.Value]++
+	}
+}
+
+// Запрашивает номер карты, удаляет карту и возвращает ее
+func (h *Hand) pick() Card {
+	cardnum := 1
+	var choice string
+	table := make(map[int]int)
+	Table(table)
+
+	fmt.Println(h.Hand) // печатаем руку
+	for {
+		fmt.Scanf("%s\n", &choice) //ожидаем номер карты
+		if choice == "-" {
+			return Card{"", 0}
+		}
+
+		cardnum, _ = strconv.Atoi(choice)
+		cardnum--
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		if cardnum >= 0 && cardnum < len(h.Hand) { // если данные введены корректно
+			_, ok := table[h.Hand[cardnum].Value]
+			if len(attackTable) <= len(defenseTable) && (ok || len(table) == 0) { // атака
+				fmt.Println("Attack")
+				fmt.Println("\033[31m", h.Hand[cardnum], "\033[0m") // печатаем карту
+				card := h.Hand[cardnum]                             // запоминаем карту
+				h.Hand = Remove(h.Hand, cardnum)                    //удаляем карту из руки
+
+				return card
+			} else if len(attackTable) > len(defenseTable) && h.Hand[cardnum].Value > attackTable[len(attackTable)-1].Value && h.Hand[cardnum].Suit == attackTable[len(attackTable)-1].Suit { //защита
+				fmt.Println("\033[31m", h.Hand[cardnum], "\033[0m") // печатаем карту
+				card := h.Hand[cardnum]                             // запоминаем карту
+				h.Hand = Remove(h.Hand, cardnum)                    //удаляем карту из руки
+
+				return card
+			}
+		}
+		fmt.Println("Please try again")
+	}
+
+}
+
+func (h *Hand) Attack(anotherHand *Hand) bool {
+	attackTable = nil
+	defenseTable = nil
+	deflen := len(anotherHand.Hand)
+	skip := false
+
+	for i := 0; i < deflen; i++ {
+		fmt.Println("\033[32mAttack: Choice card", 1, "-", len(h.Hand), "\033[0m")
+		attackTable = append(attackTable, h.pick())
+		if attackTable[len(attackTable)-1].Value == 0 {
+			fmt.Println("End of attack")
+			break
+		}
+		fmt.Println(attackTable)  //печатаем столы
+		fmt.Println(defenseTable) //печатаем столы
+
+		fmt.Println("\033[32mDefense: Choice card", 1, "-", len(anotherHand.Hand), "\033[0m")
+		defenseTable = append(defenseTable, anotherHand.pick())
+		if defenseTable[len(defenseTable)-1].Value == 0 {
+			fmt.Println("You lose")
+			anotherHand.Hand = append(anotherHand.Hand, defenseTable[:len(defenseTable)-1]...)
+			anotherHand.Hand = append(anotherHand.Hand, attackTable...)
+			skip = true
+			break
+		}
+	}
+	// добираем карты
+	h.Fill()
+	anotherHand.Fill()
+	return skip
+}
+
+//--------------------------------
 
 func skip(b bool, i *int) {
 	if b {
@@ -14,12 +163,12 @@ func skip(b bool, i *int) {
 }
 
 func main() {
-	cards.Deck = make([]cards.Card, 52)
-	cards.DeckGenerator(cards.Deck)
-
+	//web()
+	Deck = make([]Card, 52)
+	DeckGenerator()
 	countPlayers := 0 // спрашиваем сколько игроков будет
 	for {
-		fmt.Println("Enter count players")
+		fmt.Println("Enter the number of players")
 		var temp string
 		fmt.Scanf("%s\n", &temp)
 		countPlayers, _ = strconv.Atoi(temp)
@@ -28,25 +177,38 @@ func main() {
 		}
 		fmt.Println("Try again")
 	}
-	players := make([]cards.Hand, countPlayers) // создаем слайс игроков
+	players := make([]Hand, countPlayers) // создаем слайс игроков
 
 	for i := range players {
 		players[i].Fill()
 	}
-
+	fmt.Println(trumpcard)
 	// атака по кругу
 	for i := 0; ; i++ {
+		fmt.Println("Turn player", i%len(players), "under", (i+1)%len(players))
+		// if i >= len(players)-1 {
+		// 	sp := players[i].Attack(&players[0])
+		// 	i = -1
+		// 	skip(sp, &i)
 
-		fmt.Println("Turn player", i)
-		if i >= len(players)-1 {
-			sp := players[i].Attack(&players[0])
-			i = -1
-			skip(sp, &i)
-
-		} else {
-			sp := players[i].Attack(&players[i+1])
-			skip(sp, &i)
-		}
+		// } else {
+		// 	sp := players[i].Attack(&players[i+1])
+		// 	skip(sp, &i)
+		// }
+		sp := players[i%len(players)].Attack(&players[(i+1)%len(players)])
+		skip(sp, &i)
 	}
 
+}
+
+func viewHandler(writer http.ResponseWriter, equest *http.Request) {
+	test := []Card{{Suit: "heart", Value: 2}, {Suit: "heart", Value: 3}, {Suit: "heart", Value: 4}, {Suit: "heart", Value: 5}, {Suit: "heart", Value: 6}, {Suit: "heart", Value: 7}}
+	html, _ := template.ParseFiles("main.html")
+	html.Execute(writer, test)
+}
+
+func web() {
+	http.HandleFunc("/game", viewHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 }
